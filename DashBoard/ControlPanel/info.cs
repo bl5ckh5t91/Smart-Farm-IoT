1629af009791a3133e8505cac40a027a36b37e52
Abdalla I.Aburwais <114385026+bl5ckh5t91@users.noreply.github.com>
2022-09-26 14:22:54 +0200

    Add files via upload

---
+++ DashBoard/ControlPanel/info.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlPanel
+{
+    public partial class info : Form
+    {
+        public info()
+        {
+            InitializeComponent();
+        }
+
+        private void info_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'frameDataSet.info' table. You can move, or remove it, as needed.
+            this.infoTableAdapter.Fill(this.frameDataSet.info);
+
+        }
+    }
+}
